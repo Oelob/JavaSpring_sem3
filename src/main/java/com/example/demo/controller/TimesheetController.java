9661410ea20fbd350193e55e17ade1b1d973dc6d
Chernykh Ivan <114511742+Oelob@users.noreply.github.com>
2024-10-09 12:25:18 +0500

    семинар 5 добавил Сотрудников связал с timesheets

--- src/main/java/com/example/demo/controller/TimesheetController.java
+++ src/main/java/com/example/demo/controller/TimesheetController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-//@RequestMapping("/timesheets")
+
 public class TimesheetController {
 
     private final TimesheetService timesheetService;
